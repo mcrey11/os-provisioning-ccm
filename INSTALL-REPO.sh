d9130eb510d995ffc34160057c3aae28380ccc55
Ole Ernst <ole.ernst@nmsprime.com>
2022-11-09 09:16:54 +0100

    use native nodejs for genieacs with alma9

--- INSTALL-REPO.sh
+++ INSTALL-REPO.sh
@@ -6,8 +6,5 @@ yum install -y epel-release
 # add NMS Prime repos
 yum install -y https://repo.nmsprime.com/rpm/misc/nmsprime-repos-latest.noarch.rpm
 
-# enable software collections, needed for rh-nodejs12
-yum install -y centos-release-scl
-
 # clean & update
 yum clean all && yum update -y
