662974efec87e979055e6cb7d7a3bd2d1f4134bf
Ole Ernst <ole.ernst@nmsprime.com>
2024-04-05 14:58:43 +0200

    migrate codebase from php8.0 to php8.1

--- tests/BaseLifecycleTest.php
+++ tests/BaseLifecycleTest.php
@@ -763,7 +763,6 @@ class BaseLifecycleTest extends TestCase
         $refl = new \ReflectionObject($this);
         foreach ($refl->getProperties() as $prop) {
             if (! $prop->isStatic() && 0 !== strpos($prop->getDeclaringClass()->getName(), 'PHPUnit_')) {
-                $prop->setAccessible(true);
                 $prop->setValue($this, null);
             }
         }
