4f3e1992444757f417747974f214dd2fe7a19327
Ole Ernst <olebowle@gmx.com>
2024-05-23 14:41:27 +0200

    allow explicitly setting geocode source

--- app/extensions/geocoding/GeoReferencable.php
+++ app/extensions/geocoding/GeoReferencable.php
@@ -474,7 +474,7 @@ trait GeoReferencable
         if ((is_null($this->lng) && is_null($this->lat)) || ($this->exists && multi_array_key_exists(['street', 'house_nr', 'zip', 'city'], $changes))) {
             $this->geocode(false);
         } elseif (multi_array_key_exists(['lng', 'lat'], $changes)) {
-            $this->geocode_source = \App\BaseModel::getUser();
+            $this->geocode_source = request()->has('geocode_source') && $this->getDirty('geocode_source') ? request('geocode_source') : \App\BaseModel::getUser();
         }
     }
 
