99abb401211258b0f82f7403c022e444ff12809d
Ole Ernst <ole.ernst@nmsprime.com>
2024-06-10 12:41:10 +0200

    rebuild global dhcp config on domain changes

--- modules/ProvBase/Observers/ProvBaseObserver.php
+++ modules/ProvBase/Observers/ProvBaseObserver.php
@@ -49,7 +49,7 @@ class ProvBaseObserver
             Queue::pushOn('high', new \Modules\ProvBase\Jobs\DhcpJob());
         }
 
-        if (multi_array_key_exists(['dhcp_def_lease_time', 'dhcp_max_lease_time', 'max_cpe', 'provisioning_server'], $changes)) {
+        if (multi_array_key_exists(['dhcp_def_lease_time', 'dhcp_max_lease_time', 'domain_name', 'max_cpe', 'provisioning_server'], $changes)) {
             // recreate global DHCP config file
             $model->make_dhcp_glob_conf();
         }
