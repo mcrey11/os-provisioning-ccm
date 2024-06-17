f70606e12a9dfda80bdd958bdb7221cebd0815c5
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2024-06-17 16:00:54 +0200

    Fix: Remove superfluous chars in blade

--- modules/ProvBase/Resources/views/widgets/quickstart.blade.php
+++ modules/ProvBase/Resources/views/widgets/quickstart.blade.php
@@ -26,10 +26,10 @@
                     @continue
                 @endif
                 <a href="{{ str_replace("index","create",$valuearray['link']) }}">
-                    '<span class="btn btn-dark p-10 m-5 m-r-10 text-center">
+                    <span class="btn btn-dark p-10 m-5 m-r-10 text-center">
                         <i style="font-size: 25px;" class="img-center fa {{ $valuearray['icon'] }} p-10"></i><br />
                         <span class="username text-ellipsis text-center">{{ trans('view.createButtonTitle.'.str_replace(".index","",$valuearray['link'])) }}</span>
-                    </span>'))
+                    </span>
                 </a>
             @endforeach
         @endif
