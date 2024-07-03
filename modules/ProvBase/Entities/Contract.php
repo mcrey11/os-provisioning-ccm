96f4ebc66f8ada602015df586350e5bdbc7940f1
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2024-07-03 15:21:13 +0200

    Let house nr length to be consistently max 20 chars and add validation to avoid exception

--- modules/ProvBase/Entities/Contract.php
+++ modules/ProvBase/Entities/Contract.php
@@ -56,6 +56,7 @@ class Contract extends \BaseModel
             'number2' => 'nullable|string|unique:contract,number2,'.$id.',id,deleted_at,NULL',
             'number3' => 'nullable|string|unique:contract,number3,'.$id.',id,deleted_at,NULL',
             // 'number4' => 'string|unique:contract,number4,'.$id.',id,deleted_at,NULL', 	// old customer number must not be unique!
+            'house_number' => 'nullable|string|max:20',
             'salutation' => 'required',
             'company' => 'required_if:salutation,placeholder_salutations_institution',
             'firstname' => 'required_if:salutation,placeholder_salutations_person',
