21181a6e22da3205f873dcc794e43eca04fc0a92
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2024-09-30 16:43:52 +0200

    Fix translation

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -385,7 +385,7 @@ return [
     'modem_restart_error'       => 'Could not restart Modem! (offline?)',
     'Contact Persons'           => 'Contact Persons',
     'modem_restart_success_netgw' => 'Restarted modem via NetGw',
-    'modem_restart_warning_dns' => 'Restart failed, since hostname could not be resolved (modem as no active DHCP lease) to determine the responsible NetGw.',
+    'modem_restart_warning_dns' => 'Restart failed, since hostname could not be resolved (modem has no active DHCP lease) to determine the responsible NetGw.',
     'Accounting Text (optional)'=> 'Accounting Text (optional)',
     'Cost Center (optional)'    => 'Cost Center (optional)',
     'modem_restart_success_direct' => 'Restarted Modem directly via SNMP/TR069',
