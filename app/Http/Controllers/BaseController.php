0908d6a08bc5e57f105f196d15774f13a767ad39
Patrick Reichel <patrick.reichel@nmsprime.com>
2024-10-22 15:13:07 +0200

    Bool in index table: Rename array key.
    Thanks @NinoRy

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1668,7 +1668,7 @@ class BaseController extends Controller
         $model = static::get_model_obj();
         $dtConfig = $model->view_index_label();
         $headerFields = $dtConfig['index_header'];
-        $beautifyBooleanColumnsData = $dtConfig['beautifyBooleanColumns'];
+        $translateBooleanColumnsData = $dtConfig['translateBooleanColumns'];
         $joins = $dtConfig['join'] ?? [];
         $editColumnData = $dtConfig['edit'] ?? [];
         $filterColumnData = $dtConfig['filter'] ?? [];
@@ -1678,7 +1678,7 @@ class BaseController extends Controller
         $query = $model::selectRaw(implode(',', $joins ? array_merge($selectQuery, array_merge(...array_map(fn ($join) => $join['select'], $joins))) : $selectQuery));
 
         // set edit and filter values for boolean columns to make them natural language and filterable
-        foreach ($beautifyBooleanColumnsData as $column) {
+        foreach ($translateBooleanColumnsData as $column) {
             // use BaseModel::boolToLanguageString() method to translate boolean values
             $editColumnData[$column] = 'boolToLanguageString';
             // use BaseMdodel::boolToStringFilter method for filtering in datatables
