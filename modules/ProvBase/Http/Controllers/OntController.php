1a0e1fb44267cdf3daa4c1d230d93874dd755f67
Patrick Reichel <patrick.reichel@nmsprime.com>
2025-06-24 18:16:34 +0200

    Add ProvBase Ont MVC (derived from ProvBase Modem).

---
+++ modules/ProvBase/Http/Controllers/OntController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\ProvBase\Http\Controllers;
+
+/**
+ * OntController
+ *
+ * Controller for managing ONT resources, extends ModemController.
+ */
+class OntController extends ModemController
+{
+}
---
+++ modules/ProvBase/Observers/OntObserver.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\ProvBase\Observers;
+
+/**
+ * OntObserver
+ *
+ * Observer for ONT model events, extends ModemObserver.
+ */
+class OntObserver extends ModemObserver
+{
+}
