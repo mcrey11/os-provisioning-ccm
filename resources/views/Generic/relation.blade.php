d8995b7e06cdf73254ed33c4b5af45845700f644
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2025-07-11 11:25:33 +0200

    Fix: Readd button so that  array is filled and fields from relation are filled automatically again. This fixes empty fields when creating a modem that were previously filled with data from contract

--- resources/views/Generic/relation.blade.php
+++ resources/views/Generic/relation.blade.php
@@ -67,10 +67,11 @@ Relation Blade is used inside a Panel Element to display relational class object
             @endforeach
 
             <div class="col align-self-start">
-                <a href="{{ isset($options['add_url']) ? $options['add_url'] : route($class.'.create', [$key => $view_var->id]) }}"
-                   class="btn btn-outline-primary float-right m-b-10"
-                   title="{{ isset($options['create_button_text']) ? trans($options['create_button_text']) : trans('view.createButtonTitle.'.$class) }}">
-                    <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
+                <a href="{{ isset($options['add_url']) ? $options['add_url'] : route($class.'.create', [$key => $view_var->id]) }}">
+                    <button style="simple" type="submit" class="btn btn-outline-primary float-right m-b-10"
+                        title="{{ isset($options['create_button_text']) ? trans($options['create_button_text']) : trans('view.createButtonTitle.'.$class) }}">
+                        <i class="fa fa-plus fa-2x" aria-hidden="true"></i>
+                    </button>
                 </a>
             </div>
 
