9a62fadd2a37ea0c0904de9780d2eef4db607883
Nino Ryschawy <nino.ryschawy@nmsprime.com>
2026-01-08 17:59:33 +0100

    Fix review comments: typo and trailing whitespace

--- resources/views/Generic/relationTable.blade.php
+++ resources/views/Generic/relationTable.blade.php
@@ -13,7 +13,7 @@
                 <tr class="{{ $labelData['bsclass'] ?? ''}}">
                     <td width="20"> {{ Form::checkbox('ids['.$rel_elem->id.']', 1, null, null, ['style' => 'simple']) }} </td>
                     <td class="pl-2">
-                        <div class="flex items-center" >
+                        <div class="flex items-center">
                             {!! $rel_elem->view_icon() !!}
                             <a href="{{ route($class.'.'.$method, $rel_elem->id) }}" class="ml-0" title="{!! $labelData['title'] ?? '' !!}">
                                 {!! is_array($labelData) ? $labelData['header'] : $labelData !!}
